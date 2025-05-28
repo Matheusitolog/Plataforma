0ecd46541b691634ffa2170a4a4e55fc34ebfacc
Matheusitolog <comunidade.itolog@gmail.com>
2025-05-28 02:13:59 -0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -18,7 +18,7 @@ from pathlib import Path
 # ======================
 class Config:
     # Configurações da API
-    API_KEY = "MEU_CEREBRO"
+    API_KEY = "AIzaSyBh_DPFaQRTNlzpMx-08iAnbAbTD6xZ5BQ"
     API_URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={API_KEY}"
     
     # URLs de checkout/páginas
